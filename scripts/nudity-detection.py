217cdb240646916d4b75a682bf562242d08a5ab4
Yash Rathod <yrraadi@gmail.com>
2021-10-27 22:12:59 -0500

    nudity-detection.py script updated

--- scripts/nudity-detection.py
+++ scripts/nudity-detection.py
@@ -1,24 +1,43 @@
 from nudenet import NudeClassifier
+from nudenet import NudeDetector
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
 
-
-def classify(path):
+# Method to classify a given image as safe or unsafe
+def classify(img):
     classifier = NudeClassifier()
-    for images_to_classify in os.listdir(path):
-        # image = mpimg.imread(path + images_to_classify)
-        result = classifier.classify(path + images_to_classify)
-        print(images_to_classify)
-        print("Unsafe " + str(result[path + images_to_classify]["unsafe"]))
-        print("Safe " + str(result[path + images_to_classify]["safe"]))
-        # plt.imshow(image)
-        # plt.show()
+    return classifier.classify(img)
+
+
+# Detector to detect all exposed body parts in the image
+def detect(img):
+    detector = NudeDetector()
+    return detector.detect(img)
+
+
+# Method to display the images
+def displayImage(img):
+    image = mpimg.imread(img)
+    plt.imshow(image)
+    plt.show()
 
 
 def main():
     path = "C:/Users/yash4/OneDrive/Desktop/images/"
-    classify(path)
+    for images_to_classify in os.listdir(path):
+        classifierResult = classify(path + images_to_classify)
+        detectorResult = detect(path + images_to_classify)
+        # displayImage(path + images_to_classify)
+        print(images_to_classify + ":")
+        print(
+            str(classifierResult[path + images_to_classify]["safe"])
+            + " "
+            + str(classifierResult[path + images_to_classify]["unsafe"])
+        )
+        for dictionary in detectorResult:
+            print(dictionary["label"], end=" ")
+        print("\n")
 
 
 main()
