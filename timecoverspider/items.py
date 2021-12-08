b91a45447e940fa7dfcc8170060d1fc1557e9a7d
RickyChen30 <90669624+RickyChen30@users.noreply.github.com>
2021-12-08 17:45:09 -0600

    Update items.py

--- timecoverspider/items.py
+++ timecoverspider/items.py
@@ -1,7 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-class MagazineCover(scrapy.Item):
-	title = scrapy.Field()
-	pubDate = scrapy.Field()
-	file_urls = scrapy.Field()
-	files = scrapy.Field()
+
+
+class TumblrItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
